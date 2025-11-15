3a7136cdab78d65d30a132b8260052ed840190cc
Амантур Эркинов <amanturerkinov@Noutbuk-Amantur.local>
2025-11-15 16:48:35 +0600

    dostavka

--- project/urls.py
+++ project/urls.py
@@ -28,6 +28,9 @@ urlpatterns = [
 ]
 
 urlpatterns += i18n_patterns( path("", include("core.urls", namespace="core")) )
-
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
